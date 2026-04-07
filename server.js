91a39d8f606bf3fdd7f79036d393ae525f08e984
Salah8042 <164727393+Salah8042@users.noreply.github.com>
2026-04-07 20:18:44 +0200

    server.js

--- server.js
+++ server.js
@@ -3,40 +3,31 @@ const app = express();
 
 app.use(express.json());
 
+// 👇 تخزين مؤقت (يتصفر لو السيرفر عمل restart)
 let users = {};
 
-// الصفحة الرئيسية
-app.get("/", (req, res) => {
-  res.send("Server running 🔥");
+// 🎁 هدية التسجيل
+app.post("/reward", (req, res) => {
+    const { uid } = req.body;
+
+    if (!uid) {
+        return res.status(400).send("invalid");
+    }
+
+    // 👇 أول مرة بس
+    if (!users[uid]) {
+        users[uid] = 3;
+    }
+
+    res.send({ balance: users[uid] });
 });
 
-// مكافأة التسجيل
-app.post("/reward", (req, res) => {
-  const { uid } = req.body;
-
-  if (!uid) {
-    return res.status(400).send("no uid");
-  }
-
-  if (!users[uid]) {
-    users[uid] = {
-      balance: 0,
-      bonus: false
-    };
-  }
-
-  if (users[uid].bonus) {
-    return res.send("Already taken");
-  }
-
-  users[uid].balance += 3;
-  users[uid].bonus = true;
-
-  res.json({ balance: users[uid].balance });
+// 👇 اختبار السيرفر
+app.get("/", (req, res) => {
+    res.send("Server running 🔥");
 });
 
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on " + PORT);
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
